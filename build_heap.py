8ab83c7c7674b78b10ce2d0320408c5a2035f2e9
laurasturaine <125255157+laurasturaine@users.noreply.github.com>
2023-03-19 22:14:54 +0000

    updf

--- build_heap.py
+++ build_heap.py
@@ -1,29 +1,25 @@
 def build_heap(data):
-    n=len(data)
     swaps = []
+    n=len(data)
 
     for i in range(n//2,-1,-1):
-        swaps=s_down(i,data,swaps)
+        down(data,i,n,swaps)
     return swaps
 
-        #min_index=i 
-        #l=2*i+1
-        #r=2*i+2
 
-def s_down(i,data,swaps):
-    n=len(data)
+def down(data,i,n,swaps):
     min_index=i 
     l=2*i+1
     r=2*i+2
-    if l<n and data[l]<data[min_index]:
+    if l<n and data[l]<data[i]:
         min_index=l 
-    if r<n and data[r]<data[min_index]:
+    if r<n and data[r]<data[i]:
         min_index=r 
 
     if i!=min_index:
         swaps.append((i,min_index))
         data[i],data[min_index]=data[min_index],data[i]
-        swaps=s_down(min_index,data,swaps)
+        down(data,min_index,n,swaps)
            # while min_index<n//2:
                 #l=2*min_index+1
               #  r=2*min_index+2
@@ -41,36 +37,23 @@ def s_down(i,data,swaps):
 
 def main():
 
-    # input from keyboard
-    #n = int(input().strip())
-    #data = list(map(int, input().split()))
+   
 
     # checks if lenght of data is the same as the said lenght
-    sort_type=input()
+    i_type=input("enter input method I/F: ")
 
 
-    if "I" in sort_type:
-        n = int(input())
-        u_data=input()
-        data=list(map(int, u_data.split(" ")))
-        assert len(data)==n 
+    if "I" in i_type:
+        n = int(input("eneter number of elements: "))
+        data=list(map(int, input("enter the elements: ").split()))
 
-    elif "F" in sort_type:
-        fl=input()
-        if "a" in fl:
-            return
-        with open(f"./test/{fl}",mode="r") as fl:
-            n=int(fl.readline())
-            data=list(map(int, fl.readline().split(" ")))
-        assert len(data)==n 
+    if "F" in i_type:
+        fn=input("eneter the filename")
+        with open(f"tests/{fn}") as file:
+            n=int(file.readline())
+            data=list(map(int, file.readline().split()))
+    assert len(data)==n 
 
-    else:
-        return
-
-    saraksts=[[]for i in range(n)]
-    for i in range(n):
-        if data[i]<n:
-            saraksts[data[i]].append(i)
     swaps=build_heap(data)
         # output all swaps
     print(len(swaps))
