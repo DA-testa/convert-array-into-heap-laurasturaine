b786cd6b2d54d8cce8391b28fe1ad75d291467a5
laurasturaine <125255157+laurasturaine@users.noreply.github.com>
2023-03-19 20:22:02 +0000

    upd5

--- build_heap.py
+++ build_heap.py
@@ -1,32 +1,33 @@
 # python3
 
+import os
 
 def build_heap(data):
-    n=len(data)
     swaps = []
+    izm=len(data)
+  
 
-    for i in range(n//2,-1,-1):
-        swaps=s_down(i,data,swaps)
+    for i in range(izm//2,-1,-1):
+        s_down(data,i,swaps)
     return swaps
-
         #min_index=i 
         #l=2*i+1
         #r=2*i+2
 
-def s_down(i,data,swaps):
-    n=len(data)
+def s_down(data,i,swaps):
+    izm=len(data)
     min_index=i 
-    l=2*i+1
-    r=2*i+2
-    if l<n and data[l]<data[min_index]:
-        min_index=l 
-    if r<n and data[r]<data[min_index]:
-        min_index=r 
+    left=2*i+1
+    right=2*i+2
+    if left<izm and data[left]<data[min_index]:
+        min_index=left 
+    if right<izm and data[right]<data[min_index]:
+        min_index=right 
 
     if i!=min_index:
         swaps.append((i,min_index))
         data[i],data[min_index]=data[min_index],data[i]
-        swaps=s_down(min_index,data,swaps)
+        s_down(data,min_index,swaps)
            # while min_index<n//2:
                 #l=2*min_index+1
               #  r=2*min_index+2
@@ -39,7 +40,7 @@ def s_down(i,data,swaps):
                   #  data[i], data[min_index]=data[min_index], data[i]
                # else:
                 #    break
-    return swaps
+
 
 
 def main():
@@ -48,38 +49,35 @@ def main():
     #n = int(input().strip())
     #data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
-    sort_type=input()
-
-
-    if "I" in sort_type:
+    teksts=input()
+    if "I" in teksts:
         n = int(input())
-        u_data=input()
-        data=list(map(int, u_data.split(" ")))
+        data=list(map(int, input().split()))
         assert len(data)==n 
+        swaps=build_heap(data)
 
-    elif "F" in sort_type:
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
+
+    elif "F" in teksts:
         fl=input()
+        cels='./tests/'
+        ma=os.path.join(cels,fl)
         if "a" in fl:
+            print("Faila nosaukumā ir kļūda")
             return
-        with open(f"./test/{fl}",mode="r") as fl:
-            n=int(fl.readline())
-            data=list(map(int, fl.readline().split(" ")))
-        assert len(data)==n 
+        else:
+            try:
+                with open(ma, mode="r") as fl:
+                n=int(fl.readline())
+                da=list(map(int, fl.readline().split()))
+            except Exception as error:
+                print("Error", str(error))
 
     else:
+        print("Ievadiet burtu 'I' vai 'F':")
         return
-
-    saraksts=[[]for i in range(n)]
-    for i in range(n):
-        if data[i]<n:
-            saraksts[data[i]].append(i)
-    swaps=build_heap(data)
-        # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-    
 
 
 if __name__ == "__main__":
