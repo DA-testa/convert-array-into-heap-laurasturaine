a1f1665bdc86f9ddd47f5ffb7326f14cce5fdf3c
laurasturaine <125255157+laurasturaine@users.noreply.github.com>
2023-03-19 16:57:14 +0000

    update8

--- build_heap.py
+++ build_heap.py
@@ -42,7 +42,7 @@ def main():
 
 
     # input from keyboard
-    n = int(input())
+    n = int(input().strip())
     data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
